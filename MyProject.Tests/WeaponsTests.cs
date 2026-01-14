1daee302d22945c6af6373c8c141df063891e763
Ryan McArthur <zenoscope@rocketmail.com>
2026-01-15 10:43:04 +1300

    Update WeaponsTests.cs

--- MyProject.Tests/WeaponsTests.cs
+++ MyProject.Tests/WeaponsTests.cs
@@ -1,1 +1,11 @@
 
+// plyer weapon specific things: 
+      
+      // **** Weapons for wagon
+      // set up a list of weapons (should be in another class)
+      // get/set weapon list for entity (name, damage, upgrade path, type)
+      // weapon types: held (fixed point on model), 
+      // default weapons as part of entity type/default variables      
+      
+       // ***level up weapons
+      // grab 2 random weapons and one current weapon from the weapon list to upgrade  
