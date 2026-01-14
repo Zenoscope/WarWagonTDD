a0af6270072e586b8d52f0b05080f4eba5a2f57c
Ryan McArthur <zenoscope@rocketmail.com>
2026-01-15 11:01:38 +1300

    Update comments and organization in WeaponsTests
    Refactor comments and structure in WeaponsTests class.

--- MyProject.Tests/WeaponsTests.cs
+++ MyProject.Tests/WeaponsTests.cs
@@ -1,11 +1,20 @@
+using NUnit.Framework;
 
-// plyer weapon specific things: 
-      
-      // **** Weapons for wagon
-      // set up a list of weapons (should be in another class)
+namespace WeaponsTests
+{
+  // should there be bounds checking tests or should the tests 
+   public class WeaponsTests
+    {    
+
+      // Weapons for wagon
+      // set up a list of weapons, check it exists
+      // check for attributes and if they are set up.
+         
       // get/set weapon list for entity (name, damage, upgrade path, type)
+         
       // weapon types: held (fixed point on model), 
-      // default weapons as part of entity type/default variables      
+      // default weapon as part of entity type/default variables      
       
-       // ***level up weapons
-      // grab 2 random weapons and one current weapon from the weapon list to upgrade  
+       // level up weapon         
+    }
+}
