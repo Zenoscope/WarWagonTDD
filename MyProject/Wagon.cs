83df317bac416e6cff1eebefc12438fc1ce8f197
Ryan McArthur <zenoscope@rocketmail.com>
2026-01-27 09:34:35 +1300

    Create Wagon.cs

---
+++ MyProject/Wagon.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Wagon {
+  public class Wagon {
+
+playerWeapons = [
+      "Anvil", 3,50,
+      "Axe", 3,50,
+      "lightning", 3, 50,
+      "Flying hammer", 3, 50,
+      "Flying daggers", 3, 50
+      "arrows",  // area of effect
+      ];
+ 
+ // attributes 
+ wagon x and y
+ wagon character
+ active weapons
+ inactive weapons (list of weapons previolsy used, includes update status etc
+
+ // methods
+ move wagon
+ rotate wagon/wagon direction                   
+  }
+}
+    
