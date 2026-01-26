114f1178580df91aa3b54f3c841597af50522678
Ryan McArthur <zenoscope@rocketmail.com>
2026-01-27 11:41:42 +1300

    Create Main class with game flow outline
    Initial implementation of the Main class with game flow comments.

---
+++ MyProject/Main.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Main{
+  class Main {
+  	public static void Main()
+  	{
+      // blah start menu, load things in
+      
+      // set the wagon type from previous settings
+      // start the game (is it single session or does it save after each time) 
+      // load in the wagon and drive around
+      // spawn enemies - enemies die
+      // numbers go up
+      // if the player dies, restart or go back to the start menu
+      // if the player quits, go back to the start menu
+      
+  	}
+  }
+}
